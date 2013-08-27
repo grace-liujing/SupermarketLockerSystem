1d25def0344075f46943a24100c44df347873021
grace-liujing <graceliu@thoughtworks.com>
2013-08-27 13:14:17 +0800

    should get a ticket when robot first store a bag in locker

---
+++ SupermarketLockerSystem/Robot.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketLockerSystem
+{
+    public class Robot
+    {
+        public Ticket store(Bag bag)
+        {
+            var locker = new Locker();
+            return locker.Store(bag);
+        }
+    }
+}
---
+++ SupermarketLockerSystemTest/RobotTest.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketLockerSystem;
+using Xunit;
+
+namespace SupermarketLockerSystemTest
+{
+    public class RobotTest
+    {
+        
+        [Fact]
+        public void should_get_a_ticket_when_robot_fisrt_store_a_bag_in_locker()
+        {
+            var robot = new Robot();
+            var bag = new Bag();
+            Ticket ticket = robot.store(bag);
+
+            Assert.NotNull(ticket);
+        } 
+    }
+
+
+}
