4612f5d63b0e8b87348ef1edcfeb0cc18e03fca1
grace-liujing <graceliu@thoughtworks.com>
2013-08-29 22:29:53 +0800

    add smartrobot and do same test as robot

---
+++ SupermarketLockerSystem/SmartRobot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupermarketLockerSystem
+{
+    public class SmartRobot
+    {
+        private static int lockerCount;
+        private readonly List<Locker> lockers;
+
+        public SmartRobot(List<Locker> lockerList)
+        {
+            lockerCount = lockerList.Count;
+            lockers = lockerList;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            for (var num = 0; num < lockerCount; num++)
+            {
+                if (lockers[num].IsAvailable())
+                {
+                    var ticket = lockers[num].Store(bag);
+                    return ticket;
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+        public Bag Pick(Ticket ticket)
+        {
+            int num;
+            Bag bag = null;
+            for (num = 0; num <= lockerCount; num++)
+            {
+                try
+                {
+                    bag = lockers[num].Pick(ticket);
+                    break;
+                }
+                catch (InvalidOperationException e)
+                {
+                }
+            }
+            return bag;
+        }
+    }
+}
---
+++ SupermarketLockerSystemTest/SmartRobotTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using SupermarketLockerSystem;
+using Xunit;
+
+namespace SupermarketLockerSystemTest
+{
+    public class SmartRobotTest
+    {
+        private readonly SmartRobot smarRobot = new SmartRobot(new List<Locker>()
+            {
+                new Locker(1),
+                new Locker(2)
+            });
+
+        [Fact]
+        public void should_get_a_ticket_when_smartrobot_store_a_bag_in_locker()
+        {
+            var bag = new Bag();
+            Ticket ticket = smarRobot.Store(bag);
+
+            Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void should_pick_bag_when_smartrobot_uses_right_ticket()
+        {
+            var bag = new Bag();
+            Ticket ticket = smarRobot.Store(bag);
+            Bag pickdBag = smarRobot.Pick(ticket);
+
+            Assert.Same(bag, pickdBag);
+        }
+
+        [Fact]
+        public void should_store_bags_as_many_as_the_total_capacity()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                Ticket ticket = smarRobot.Store(new Bag());
+                Assert.NotNull(ticket);
+            }
+        }
+    }
+
+
+}
