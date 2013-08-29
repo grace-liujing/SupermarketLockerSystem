80ab95e9fe0f700aac0f5d165d530415cf4c8341
grace-liujing <graceliu@thoughtworks.com>
2013-08-29 09:19:03 +0800

    change locker to support multiple bags

--- SupermarketLockerSystem/Locker.cs
+++ SupermarketLockerSystem/Locker.cs
@@ -5,19 +5,28 @@ namespace SupermarketLockerSystem
 {
     public class Locker
     {
-        private Dictionary<Ticket, Bag> storedBagMap; 
-        public bool IsEmpty { get; private set;}
-        private int Capacity { get; set; }
+        private Dictionary<Ticket, Bag> storedBagMap;
+        public bool IsEmpty { get; private set; }
+        public int Capacity { get; set; }
+        public int AvailableCount { get; private set; }
 
         public Locker(int boxCount)
         {
+            storedBagMap = new Dictionary<Ticket, Bag>();
             Capacity = boxCount;
+            AvailableCount = boxCount;
             IsEmpty = true;
         }
 
         public Ticket Store(Bag bag)
         {
+            if (AvailableCount == 0)
+            {
+                throw new InvalidOperationException();
+            }
             var ticket = new Ticket();
+            storedBagMap[ticket] = bag;
+            AvailableCount--;
             IsEmpty = false;
             return ticket;
         }
@@ -25,16 +34,15 @@ namespace SupermarketLockerSystem
         public Bag Pick(Ticket ticket)
         {
             if (ticket == null) throw new ArgumentNullException();
-            
-            Bag bag = storedBagMap[ticket];
-            if (bag!=null)
-            {
-                IsEmpty = true;
-                storedBagMap.Remove(ticket);
-                return bag;
-            }
 
-            throw new InvalidOperationException();
+            if (!storedBagMap.ContainsKey(ticket))
+            {
+                throw new InvalidOperationException();
+            }
+            Bag bag = storedBagMap[ticket];
+            IsEmpty = true;
+            storedBagMap.Remove(ticket);
+            return bag;
         }
     }
 }
--- SupermarketLockerSystemTest/LockerTest.cs
+++ SupermarketLockerSystemTest/LockerTest.cs
@@ -1,18 +1,33 @@
-﻿using SupermarketLockerSystem;
+﻿using System;
+using SupermarketLockerSystem;
 using Xunit;
 
 namespace SupermarketLockerSystemTest
 {
     public class LockerTest
     {
-        private readonly Locker _locker = new Locker(4);
+        private readonly Locker locker = new Locker(4);
 
         [Fact]
         public void should_get_a_ticket_when_store_a_bag_into_locker()
         {
             var bag = new Bag();
-            Ticket ticket = _locker.Store(bag);
+            Ticket ticket = locker.Store(bag);
             Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void should_return_right_capacity_when_initialize()
+        {
+            Assert.Equal(4, locker.Capacity);
+        }
+
+        [Fact]
+        public void should_get_right_capacity_after_store_a_bag_into_locker()
+        {
+            var bag = new Bag();
+            locker.Store(bag);
+            Assert.Equal(3, locker.AvailableCount);
         }
 
         [Fact]
@@ -20,61 +35,73 @@ namespace SupermarketLockerSystemTest
         {
             var bag = new Bag();
             var anotherBag = new Bag();
-            _locker.Store(bag);
-            Ticket ticket = _locker.Store(anotherBag);
+            locker.Store(bag);
+            Ticket ticket = locker.Store(anotherBag);
             Assert.NotNull(ticket);
         }
 
-//        [Fact] 
-//        public void should_get_ticket_when_store_nothing()
-//        {
-//            Ticket ticket = _locker.Store(null);
-//            Assert.NotNull(ticket);
-//        }
-//
-//        [Fact]
-//        public void should_not_store_another_bag_when_nothing_is_stored_in_locker()
-//        {
-//            var bag = new Bag();
-//            _locker.Store(null);
-//            Assert.Throws<InvalidOperationException>(() => _locker.Store(bag));
-//        }
-//
-//        [Fact]
-//        public void should_pick_the_bag_from_locker_when_use_right_ticket()
-//        {
-//            var bag = new Bag();
-//            Ticket ticket = _locker.Store(bag);
-//            Bag pickBag = _locker.Pick(ticket);
-//
-//            Assert.Equal(bag,pickBag);
-//        }
-//
-//        [Fact]
-//        public void should_not_pick_the_bag_from_locker_when_null_ticket_is_provided()
-//        {
-//            var bag = new Bag();
-//            _locker.Store(bag);
-//
-//            Assert.Throws<ArgumentNullException>(() => _locker.Pick(null));
-//        }
-//
-//        [Fact]
-//        public void should_not_pick_the_bag_from_locker_when_use_invalidate_ticket()
-//        {
-//            var bag = new Bag();
-//            _locker.Store(bag);
-//            var ticket = new Ticket();
-//
-//            Assert.Throws<InvalidOperationException>(() => _locker.Pick(ticket));
-//        }
-//
-//        [Fact]
-//        public void should_not_pick_the_bag_from_locker_before_storing_the_bag()
-//        {
-//            var ticket = new Ticket();
-//
-//            Assert.Throws<InvalidOperationException>(() => _locker.Pick(ticket));
-//        }
+        [Fact]
+        public void should_throw_exception_when_store_bag_if_locker_is_full()
+        {
+            for (int i = 0; i < locker.Capacity; i++)
+            {
+                locker.Store(new Bag());
+            }
+            Assert.Throws<InvalidOperationException>(() => locker.Store(new Bag()));
+        }
+
+        [Fact] 
+        public void should_get_ticket_when_store_nothing()
+        {
+            Ticket ticket = locker.Store(null);
+            Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void should_pick_the_bag_from_locker_when_use_right_ticket()
+        {
+            var bag = new Bag();
+            Ticket ticket = locker.Store(bag);
+            Bag pickBag = locker.Pick(ticket);
+
+            Assert.Equal(bag,pickBag);
+        }
+
+        [Fact]
+        public void should_not_pick_the_bag_from_locker_when_null_ticket_is_provided()
+        {
+            var bag = new Bag();
+            locker.Store(bag);
+
+            Assert.Throws<ArgumentNullException>(() => locker.Pick(null));
+        }
+
+        [Fact]
+        public void should_not_pick_the_bag_from_locker_when_use_another_ticket()
+        {
+            var bag = new Bag();
+            locker.Store(bag);
+            var ticket = new Ticket();
+
+            Assert.Throws<InvalidOperationException>(() => locker.Pick(ticket));
+        }       
+        
+        [Fact]
+        public void should_not_pick_the_bag_from_locker_when_use_used_ticket()
+        {
+            var bag = new Bag();
+            Ticket ticket = locker.Store(bag);
+            locker.Pick(ticket);
+
+            Assert.Throws<InvalidOperationException>(() => locker.Pick(ticket));
+        }
+
+        [Fact]
+        public void should_not_pick_the_bag_from_locker_before_storing_the_bag()
+        {
+            var ticket = new Ticket();
+
+            Assert.Throws<InvalidOperationException>(() => locker.Pick(ticket));
+        }
     }
 }
